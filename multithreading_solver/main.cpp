f3edd41e9073b7d5eef284beaa093134cc364778
crazyZhang <917724035@qq.com>
2021-03-21 23:09:22 +0800

    read input from stdin, instead of hard coding

--- multithreading_solver/main.cpp
+++ multithreading_solver/main.cpp
@@ -93,54 +93,15 @@ std::optional<std::vector<Direction>> Solve(const Solver<5,5>::Board& board)
     return (*solver)();
 }
 
-std::vector<Solver<5, 5>::Board> boards =
-{
-    Solver<5,5>::MakeBoard
-    ({
-        0,  1,  2,  3,  4,
-        5,  6,  7,  8,  9,
-        10, 11, 12, 13, 14,
-        15, 16, 17, 18, 19,
-        20, 21, 22, 23, 24
-    }),
-    Solver<5,5>::MakeBoard
-    ({
-        0,  1,  2,  3,  4,
-        5,  6,  7,  8,  9,
-        10, 11, 12, 24, 14,
-        15, 16, 17, 18, 19,
-        20, 21, 22, 23, 13
-    }),
-    Solver<5,5>::MakeBoard
-    ({
-        0,  1,  2,  3,  4,
-        5,  11, 7,  8,  9,
-        10, 6,  12, 24, 14,
-        15, 21, 17, 18, 19,
-        20, 16, 22, 23, 13
-    }),
-    Solver<5,5>::MakeBoard
-    ({
-        0,  6,  2,  3,  4,
-        5,  1,  7,  8,  9,
-        10, 11, 12, 24, 14,
-        15, 21, 17, 18, 19,
-        20, 16, 22, 23, 13
-    }),
-    Solver<5,5>::MakeBoard
-    ({
-        0,  24, 2,  9,  4,
-        5,  6,  7,  3,  8,
-        10, 11, 12, 18, 14,
-        15, 21, 22, 13, 19,
-        20, 16, 23, 17, 1
-    })
-};
-
 int main()
 {
-    auto board = boards[3];
+    std::string in;
+    std::copy(std::istream_iterator<char>(std::cin), std::istream_iterator<char>(), std::back_inserter(in));
+
+    auto task = ToTask(in);
+    auto board = Solver<5, 5>::MakeBoard(task.board);
     auto steps = Solve(board);
+
     if (!steps)
         std::cout<<"null"<<std::endl;
     else
