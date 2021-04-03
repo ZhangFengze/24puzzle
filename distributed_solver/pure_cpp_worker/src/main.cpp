17930ac8bd17b1b3ba5265893e411eea1c05e459
crazyZhang <917724035@qq.com>
2021-04-03 18:27:28 +0800

    restructure pure_cpp_worker

---
+++ distributed_solver/pure_cpp_worker/src/main.cpp
@@ -0,0 +1,29 @@
+#include "entrypoint.h"
+#include "handler.h"
+#include "solver.h"
+#include "adapter.h"
+
+namespace Aliyun {
+namespace FC {
+namespace Handlers {
+class PuzzleHandler : public FcHttpBaseHandler
+{
+public:
+   void OnInvoke(const FcContext& context, const Pistache::Http::Request& req,
+            Pistache::Http::ResponseWriter& response) override
+            {
+                auto task = ToTask(req.body());
+                auto board = n_puzzle_solver::impl::Solver<5, 5>::MakeBoard(task.board);
+                auto tempSteps = Map(task.steps, [](int dir) {return n_puzzle_solver::Direction(dir); });
+                auto steps = n_puzzle_solver::impl::Solver<5, 5>::Solve(board, tempSteps, task.depth);
+                auto result = steps? ToJson(Map(*steps, [](const auto& dir) {return (int)dir; })) : "null";
+                response.send(Http::Code::Ok, EchoHttpHandler::mInitHandler + result);
+            }
+};
+}}}
+
+using namespace Aliyun::FC::Handlers;
+void SetInvokeAndInitHander()
+{
+    CustomRuntimeHandler::httpHandler = new PuzzleHandler();
+}
