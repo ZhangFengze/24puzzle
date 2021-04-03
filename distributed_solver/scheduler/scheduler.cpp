3380f512a5b70a7baa1576e98e5f4424a930784b
crazyZhang <917724035@qq.com>
2021-04-03 21:26:55 +0800

    remove scheduler's local worker

--- distributed_solver/scheduler/scheduler.cpp
+++ distributed_solver/scheduler/scheduler.cpp
@@ -3,15 +3,11 @@
 #include "solver.h"
 #include "async_http_client.h"
 #include "adapter.h"
-#include "boost/process.hpp"
 #include "boost/asio.hpp"
 #include "boost/asio/use_future.hpp"
 #include "boost/thread/future.hpp"
-#include <semaphore>
-#include <atomic>
 
 using namespace n_puzzle_solver::impl;
-namespace bp = boost::process;
 namespace ba = boost::asio;
 
 class Producer
@@ -49,93 +45,7 @@ private:
     std::atomic_int index = 0;
 };
 
-template<int p1, int p2, typename Type>
-struct async_out_future : ::boost::process::detail::windows::handler_base_ext,
-    ::boost::process::detail::windows::require_io_context,
-    ::boost::process::detail::uses_handles
-{
-    std::shared_ptr<boost::process::async_pipe> pipe;
-    std::shared_ptr<boost::promise<Type>> promise = std::make_shared<boost::promise<Type>>();
-    std::shared_ptr<boost::asio::streambuf> buffer = std::make_shared<boost::asio::streambuf>();
-
-    ::boost::winapi::HANDLE_ get_used_handles() const
-    {
-        return std::move(*pipe).sink().native_handle();
-    }
-
-
-    async_out_future(boost::unique_future<Type>& fut)
-    {
-        fut = promise->get_future();
-    }
-    template <typename Executor>
-    inline void on_success(Executor&)
-    {
-        auto pipe_ = this->pipe;
-        auto buffer_ = this->buffer;
-        auto promise_ = this->promise;
-        std::move(*pipe_).sink().close();
-        boost::asio::async_read(*pipe_, *buffer_,
-            [pipe_, buffer_, promise_](const boost::system::error_code& ec, std::size_t)
-            {
-                if (ec && (ec.value() != ::boost::winapi::ERROR_BROKEN_PIPE_))
-                {
-                    std::error_code e(ec.value(), std::system_category());
-                    //promise_->set_exception(std::make_exception_ptr(process_error(e)));
-                }
-                else
-                {
-                    std::istream is(buffer_.get());
-                    Type arg;
-                    if (buffer_->size() > 0)
-                    {
-                        arg.resize(buffer_->size());
-                        is.read(&*arg.begin(), buffer_->size());
-                    }
-
-                    promise_->set_value(std::move(arg));
-
-
-                }
-            });
-        this->pipe = nullptr;
-        this->buffer = nullptr;
-        this->promise = nullptr;
-
-
-    }
-
-    template<typename Executor>
-    void on_error(Executor&, const std::error_code&) const
-    {
-        std::move(*pipe).sink().close();
-    }
-
-    template <typename WindowsExecutor>
-    void on_setup(WindowsExecutor& exec)
-    {
-        if (!pipe)
-            pipe = std::make_shared<boost::process::async_pipe>(get_io_context(exec.seq));
-
-        apply_out_handles(exec, std::move(*pipe).sink().native_handle(),
-            std::integral_constant<int, p1>(), std::integral_constant<int, p2>());
-    }
-};
-
-boost::unique_future<std::string>
-AsyncSolve_Local(const Task& task, ba::io_service& ios)
-{
-    std::string input = ToJson(task);
-
-    bp::opstream in;
-    boost::unique_future<std::string> out;
-    auto child = bp::child("worker.exe", bp::std_in < in, async_out_future<1, -1, std::string>(out), ios);
-    in << input << std::endl << std::endl;
-    child.detach();
-    return out;
-}
-
-auto AsyncSolve_Http(const Task& task, ba::io_service& ios)
+auto AsyncSolve(const Task& task, ba::io_service& ios)
 {
     return AsyncHttpRequest(ios, "1045481767726147.cn-beijing.fc.aliyuncs.com", "80", "/2016-08-15/proxy/puzzle/test/", ToJson(task));
 }
@@ -159,7 +69,7 @@ int main()
     std::function<void(const Task&)> ContinuouslyAsyncSolve;
     ContinuouslyAsyncSolve = [&](const Task& task)
     {
-        AsyncSolve_Http(task, ios).then(
+        AsyncSolve(task, ios).then(
             [&, task](auto f)
             {
                 auto _ = f.get();
