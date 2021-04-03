82e7854cde0430514f06d1e248a21330497fe7da
CrazyZZZ <917724035@qq.com>
2021-04-03 18:09:36 +0800

    pure cpp solver handler

--- distributed_solver/pure_cpp_worker/sample/src/handlers/echo_handler.cpp
+++ distributed_solver/pure_cpp_worker/sample/src/handlers/echo_handler.cpp
@@ -1,4 +1,6 @@
 #include "echo_handler.h"
+#include "solver.h"
+#include "adapter.h"
 using namespace std;
 using namespace Pistache;
 
@@ -20,7 +22,12 @@ std::string EchoHttpHandler::mInitHandler;
 void EchoHttpHandler::OnInvoke(const FcContext& context, const Pistache::Http::Request& req,
             Pistache::Http::ResponseWriter& response)
 {
-    response.send(Http::Code::Ok, EchoHttpHandler::mInitHandler + req.body());
+    auto task = ToTask(req.body());
+    auto board = n_puzzle_solver::impl::Solver<5, 5>::MakeBoard(task.board);
+    auto tempSteps = Map(task.steps, [](int dir) {return n_puzzle_solver::Direction(dir); });
+    auto steps = n_puzzle_solver::impl::Solver<5, 5>::Solve(board, tempSteps, task.depth);
+    auto result = steps? ToJson(Map(*steps, [](const auto& dir) {return (int)dir; })) : "null";
+    response.send(Http::Code::Ok, EchoHttpHandler::mInitHandler + result);
 } 
 
 void EchoHttpHandler::OnInitialize(const FcContext& context)
