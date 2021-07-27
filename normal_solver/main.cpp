286fa0267a1e9c8d3be35e6da9d8630c36df8da0
crazyZhang <917724035@qq.com>
2021-07-27 21:44:53 +0800

    normal_solver now respects task.steps and task.depth

--- normal_solver/main.cpp
+++ normal_solver/main.cpp
@@ -1,23 +1,13 @@
 #include "solver.hpp"
 #include "adapter.hpp"
-#include <string>
-
-std::optional<std::vector<puzzle::Direction>> Solve(const puzzle::Solver<5, 5>::Board& board)
-{
-    for (int depth = 0;;++depth)
-    {
-        std::vector<puzzle::Direction> steps_;
-        auto steps = puzzle::Solver<5, 5>::Solve(board, steps_, depth);
-        if (steps)
-            return steps;
-    }
-}
+#include <iostream>
 
 int main()
 {
     auto task = ToTask(ReadAll(std::cin));
     auto board = puzzle::Solver<5, 5>::MakeBoard(task.board);
-    auto steps = Solve(board);
+    auto tempSteps = Map(task.steps, [](int dir) {return puzzle::Direction(dir); });
+    auto steps = puzzle::Solver<5, 5>::Solve(board, tempSteps, task.depth);
     std::cout << ToJson(steps);
     return 0;
 }
