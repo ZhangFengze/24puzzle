65144659fbaf88ead81e547dd9cb09a66ff1c043
crazyZhang <917724035@qq.com>
2021-07-27 21:54:54 +0800

    tempSteps -> historySteps

--- normal_solver/main.cpp
+++ normal_solver/main.cpp
@@ -6,8 +6,8 @@ int main()
 {
     auto task = ToTask(ReadAll(std::cin));
     auto board = puzzle::Solver<5, 5>::MakeBoard(task.board);
-    auto tempSteps = Map(task.steps, [](int dir) {return puzzle::Direction(dir); });
-    auto steps = puzzle::Solver<5, 5>::Solve(board, tempSteps, task.depth);
+    auto historySteps = Map(task.steps, [](int dir) {return puzzle::Direction(dir); });
+    auto steps = puzzle::Solver<5, 5>::Solve(board, historySteps, task.depth);
     std::cout << ToJson(steps);
     return 0;
 }
