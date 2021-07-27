d62b3e1da8555dfa002da32bb1f9d117c63614d8
crazyZhang <917724035@qq.com>
2021-07-27 21:55:51 +0800

    multithreading_solver now respects task.steps

--- multithreading_solver/main.cpp
+++ multithreading_solver/main.cpp
@@ -94,7 +94,8 @@ int main()
 {
     auto task = ToTask(ReadAll(std::cin));
     auto board = puzzle::Solver<5, 5>::MakeBoard(task.board);
-    auto steps = Solve(board, {});
+    auto historySteps = Map(task.steps, [](int dir) {return puzzle::Direction(dir); });
+    auto steps = Solve(board, historySteps);
     std::cout << ToJson(steps);
     return 0;
 }
