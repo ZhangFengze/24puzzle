17f845a04c6f56b8658c4275096b168e3c00add1
CrazyZ <917724035@qq.com>
2021-08-01 20:54:51 +0800

    python_cpp_scheduler

---
+++ distributed_solver/scheduler3/func.cpp
@@ -0,0 +1,72 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h> 
+#include "solver.hpp"
+#include "adapter.hpp"
+
+PyObject* ToPy(const puzzle::Solver<5, 5>::Board& board)
+{
+    auto list = PyList_New(0);
+    for (const auto& position : board.board)
+        PyList_Append(list, PyLong_FromLong(position.index));
+    return list;
+}
+
+PyObject* ToPy(const std::vector<puzzle::Direction>& vec)
+{
+    auto list = PyList_New(0);
+    for (auto dir : vec)
+        PyList_Append(list, PyLong_FromLong((long)dir));
+    return list;
+}
+
+PyObject* ToPy(const puzzle::Solver<5, 5>::Task& task)
+{
+    auto dict = PyDict_New();
+    PyDict_SetItem(dict, PyUnicode_FromString("board"), ToPy(task.board));
+    PyDict_SetItem(dict, PyUnicode_FromString("steps"), ToPy(task.steps));
+    return dict;
+}
+
+PyObject* GenerateTasks(PyObject* self, PyObject* args)
+{
+    const char* rawTask;
+    int preferredCount;
+    if (!PyArg_ParseTuple(args, "si", &rawTask, &preferredCount))
+        Py_RETURN_NONE;
+    auto task = ToTask(rawTask);
+    auto tasks = puzzle::Solver<5, 5>::GenerateTasks(task.board, task.steps, preferredCount);
+
+    auto result = PyList_New(0);
+    for (const auto& task : tasks)
+        PyList_Append(result, ToPy(task));
+
+    return result;
+}
+
+static PyMethodDef methods[] =
+{
+    {
+        "GenerateTasks",
+        &GenerateTasks,
+        METH_VARARGS,
+        NULL 
+    },
+    {
+        NULL, NULL, 0, NULL
+    }
+};
+
+static PyModuleDef module =
+{
+    PyModuleDef_HEAD_INIT,
+    "func",
+    NULL,
+    -1,
+    methods
+};
+
+PyMODINIT_FUNC
+PyInit_func()
+{
+    return PyModule_Create(&module);
+}
---
+++ distributed_solver/scheduler3/index.py
@@ -0,0 +1,55 @@
+import sys
+import aiohttp
+import asyncio
+import json
+import func
+
+
+class Queue:
+    queue = []
+    index = 0
+
+    def __init__(self, task: str, preferredCount: int):
+        self.tasks = func.GenerateTasks(task, preferredCount)
+
+    def Dequeue(self):
+        if not self.queue:
+            depth = self.index // len(self.tasks)
+            index = self.index % len(self.tasks)
+            self.index=self.index+1
+
+            return {
+                "board": self.tasks[index]["board"],
+                "steps": self.tasks[index]["steps"],
+                "depth": depth
+            }
+        else:
+            return self.queue.pop(0)
+
+    def Enqueue(self, task: str):
+        self.queue.append(task)
+
+
+async def worker(queue: Queue, url: str):
+    while True:
+        task = queue.Dequeue()
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, json=task) as resp:
+                result = await resp.text()
+                if result != "null":
+                    return result
+                # TODO error
+
+
+async def main(url, concurrency, taskPreferredCount, task):
+    queue = Queue(task, taskPreferredCount)
+    workers = [asyncio.create_task(worker(queue, url))
+               for _ in range(concurrency)]
+    done, _ = await asyncio.wait(workers, return_when=asyncio.FIRST_COMPLETED)
+    print(done.pop().result())
+
+
+if __name__ == "__main__":
+    config = json.loads(sys.stdin.read())
+    asyncio.run(main(config["url"], config["concurrency"],
+                config["taskPreferredCount"], json.dumps(config["task"])))
