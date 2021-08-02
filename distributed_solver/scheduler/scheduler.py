3ebb1cd8f9ed382fe4e4cb8f7c9efc3058dcbc13
crazyZhang <917724035@qq.com>
2021-08-02 18:10:49 +0800

    scheduler now respects task.depth

--- distributed_solver/scheduler/scheduler.py
+++ distributed_solver/scheduler/scheduler.py
@@ -9,12 +9,16 @@ import func
 class Queue:
     queue = []
     index = 0
+    maxIndex = 0
 
-    def __init__(self, task: str, preferredCount: int):
-        self.tasks = func.GenerateTasks(task, preferredCount)
+    def __init__(self, task, preferredCount):
+        self.tasks = func.GenerateTasks(json.dumps(task), preferredCount)
+        self.maxIndex = len(self.tasks)*task["depth"]
 
     def Dequeue(self):
         if not self.queue:
+            if self.index >= self.maxIndex:
+                return None
             depth = self.index // len(self.tasks)
             index = self.index % len(self.tasks)
             self.index = self.index+1
@@ -34,6 +38,8 @@ class Queue:
 async def worker(queue: Queue, url: str):
     while True:
         task = queue.Dequeue()
+        if not task:
+            return
         async with aiohttp.ClientSession() as session:
             async with session.post(url, json=task) as resp:
                 result = await resp.text()
@@ -46,8 +52,12 @@ async def main(url, concurrency, taskPreferredCount, task):
     queue = Queue(task, taskPreferredCount)
     workers = [asyncio.create_task(worker(queue, url))
                for _ in range(concurrency)]
-    done, _ = await asyncio.wait(workers, return_when=asyncio.FIRST_COMPLETED)
-    print(done.pop().result())
+    for coro in asyncio.as_completed(workers):
+        earliest = await coro
+        if earliest:
+            print(earliest)
+            return
+    print("null")
 
 
 if __name__ == "__main__":
@@ -55,4 +65,4 @@ if __name__ == "__main__":
     if os.name == "nt":
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(main(config["url"], config["concurrency"],
-                     config["taskPreferredCount"], json.dumps(config["task"])))
+                     config["taskPreferredCount"], config["task"]))
