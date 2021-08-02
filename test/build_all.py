0585c1507d64e06cb32808a4ad474f5425c67965
crazyZhang <917724035@qq.com>
2021-08-02 19:06:09 +0800

    fix build_all.py

--- test/build_all.py
+++ test/build_all.py
@@ -5,10 +5,8 @@ from common import print_red
 projects = (
     "normal_solver",
     "multithreading_solver",
-    "distributed_solver/aliyun/pure_cpp_worker",
-    "distributed_solver/aliyun/python_cpp_worker",
+    "distributed_solver/aliyun_worker",
     "distributed_solver/scheduler",
-    "distributed_solver/scheduler2",
 )
 
 configs = (
