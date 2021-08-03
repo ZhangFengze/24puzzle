529fa7103c947a7e1db37f792e383ba26ca661eb
crazyZhang <917724035@qq.com>
2021-08-03 10:28:23 +0800

    python script main guard

--- test/build.py
+++ test/build.py
@@ -2,23 +2,24 @@ import argparse
 from common import get_root_path, rmdir, ensure_dir_exists, cmake, build, install
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("project", help="which project to build")
-parser.add_argument("--clean", help="clean before build", action="store_true")
-parser.add_argument("--config", help="configuration", default="debug")
-parser.add_argument("--ninja", help="build using ninja", action="store_true")
-args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("project", help="which project to build")
+    parser.add_argument("--clean", help="clean before build", action="store_true")
+    parser.add_argument("--config", help="configuration", default="debug")
+    parser.add_argument("--ninja", help="build using ninja", action="store_true")
+    args = parser.parse_args()
 
-source_dir = get_root_path().joinpath(args.project)
-build_dir = get_root_path().joinpath(f"build/{args.project}")
-install_dir = get_root_path().joinpath(f"bin/{args.project}/{args.config}")
+    source_dir = get_root_path().joinpath(args.project)
+    build_dir = get_root_path().joinpath(f"build/{args.project}")
+    install_dir = get_root_path().joinpath(f"bin/{args.project}/{args.config}")
 
-if args.clean:
-    rmdir(build_dir)
-    rmdir(install_dir)
-    exit()
+    if args.clean:
+        rmdir(build_dir)
+        rmdir(install_dir)
+        exit()
 
-ensure_dir_exists(install_dir)
-cmake(source_dir, build_dir, args.ninja, install_dir)
-build(build_dir, args.config)
-install(build_dir, args.config)
+    ensure_dir_exists(install_dir)
+    cmake(source_dir, build_dir, args.ninja, install_dir)
+    build(build_dir, args.config)
+    install(build_dir, args.config)
--- test/build_all.py
+++ test/build_all.py
@@ -2,20 +2,21 @@ import subprocess
 import itertools
 from common import print_red
 
-projects = (
-    "normal_solver",
-    "multithreading_solver",
-    "distributed_solver/aliyun_worker",
-    "distributed_solver/scheduler",
-)
+if __name__ == "__main__":
+    projects = (
+        "normal_solver",
+        "multithreading_solver",
+        "distributed_solver/aliyun_worker",
+        "distributed_solver/scheduler",
+    )
 
-configs = (
-    "debug",
-    "release"
-)
+    configs = (
+        "debug",
+        "release"
+    )
 
-for project, config in itertools.product(projects, configs):
-    result = subprocess.call(
-        f"python3 test/build.py {project} --config {config}", shell=True)
-    if result != 0:
-        print_red(f"build {project} {config} failed!")
+    for project, config in itertools.product(projects, configs):
+        result = subprocess.call(
+            f"python3 test/build.py {project} --config {config}", shell=True)
+        if result != 0:
+            print_red(f"build {project} {config} failed!")
