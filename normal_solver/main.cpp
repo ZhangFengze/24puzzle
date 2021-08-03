7fb2eaa27bba8af6fee1820894f9c1dacb306bf7
crazyZhang <917724035@qq.com>
2021-08-03 11:14:05 +0800

    normal_solver should promise min steps

--- normal_solver/main.cpp
+++ normal_solver/main.cpp
@@ -2,13 +2,23 @@
 #include "adapter.hpp"
 #include <iostream>
 
+std::optional<std::vector<puzzle::Direction>> Solve(const Task& task)
+{
+    for (int maxSteps = 0;maxSteps < task.maxSteps;++maxSteps)
+    {
+        auto historySteps = task.steps;
+        auto steps = puzzle::Solver<5, 5>::Solve(task.board, historySteps, maxSteps);
+        if (steps)
+            return steps;
+    }
+    return std::nullopt;
+}
+
 int main()
 {
     auto task = ToTask(ReadAll(std::cin));
     bool solvable = puzzle::Solver<5, 5>::Solvable(task.board);
-    auto steps = solvable ?
-        puzzle::Solver<5, 5>::Solve(task.board, task.steps, task.maxSteps) :
-        std::nullopt;
+    auto steps = solvable ? Solve(task) : std::nullopt;
     std::cout << ToJson(steps);
     return 0;
 }
