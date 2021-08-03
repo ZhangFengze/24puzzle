09d9c386517bc464320ea95d23638fe5a0b319d0
crazyZhang <917724035@qq.com>
2021-08-03 10:29:21 +0800

    format code

--- test/build.py
+++ test/build.py
@@ -5,9 +5,11 @@ from common import get_root_path, rmdir, ensure_dir_exists, cmake, build, instal
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("project", help="which project to build")
-    parser.add_argument("--clean", help="clean before build", action="store_true")
+    parser.add_argument(
+        "--clean", help="clean before build", action="store_true")
     parser.add_argument("--config", help="configuration", default="debug")
-    parser.add_argument("--ninja", help="build using ninja", action="store_true")
+    parser.add_argument("--ninja", help="build using ninja",
+                        action="store_true")
     args = parser.parse_args()
 
     source_dir = get_root_path().joinpath(args.project)
