c4601548116911baef777e9b778cf62dd3672b63
CrazyZ <917724035@qq.com>
2021-08-07 22:07:26 +0800

    simplify build.py

--- test/build.py
+++ test/build.py
@@ -4,17 +4,14 @@ from common import get_root_path, rmdir, ensure_dir_exists, cmake, build, instal
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--project", help="which project to build", default="src")
     parser.add_argument(
         "--clean", help="clean before build", action="store_true")
     parser.add_argument("--config", help="configuration", default="release")
-    parser.add_argument("--ninja", help="build using ninja",
-                        action="store_true")
     args = parser.parse_args()
 
-    source_dir = get_root_path().joinpath(args.project)
-    build_dir = get_root_path().joinpath(f"build/{args.project}")
-    install_dir = get_root_path().joinpath(f"bin/{args.project}/{args.config}")
+    source_dir = get_root_path().joinpath("src")
+    build_dir = get_root_path().joinpath(f"build")
+    install_dir = get_root_path().joinpath(f"bin/{args.config}")
 
     if args.clean:
         rmdir(build_dir)
@@ -22,6 +19,6 @@ if __name__ == "__main__":
         exit()
 
     ensure_dir_exists(install_dir)
-    cmake(source_dir, build_dir, args.ninja, install_dir)
+    cmake(source_dir, build_dir, False, install_dir)
     build(build_dir, args.config)
     install(build_dir, args.config)
